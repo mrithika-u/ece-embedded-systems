87a07148bcf550db16f044ea0e1048e45c47c15f
Mrithika <mrithikaumamahesh005@gmail.com>
2026-05-04 21:40:46 +0530

    Pointer swap is frustrating

---
+++ C/SwapPointers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int temp;
+    int *p;
+    int *q;
+
+    printf("%d, %d\n", a, b);
+
+  /*  p = &a;
+    temp = *p;
+    p = &b;
+    a = *p;
+    p = &temp;
+    b = *p;*/
+
+    /*p = &a;
+    q = &b;
+    b = *p;
+    a = *q;
+
+    printf("%d, %d", a, b);
+*/
+
+//not complete yet :(
+
+}
+
+
+
+// swap two numbers using pointers
