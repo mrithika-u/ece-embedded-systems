ebdf92241a83c784a0ca1691a7e4fb5561cb2538
Mrithika <mrithikaumamahesh005@gmail.com>
2026-05-05 20:03:47 +0530

    File Handling with Struct

---
+++ C/StructureFileStudent.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+struct student
+{
+    char name[20];
+    char subject[20];
+    int marks;
+};
+int main()
+{
+    int n;
+    int i;
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    struct student s[n];
+    printf("\nEnter details of each student: \n");
+    for(i = 0; i <n; i++)
+    {
+        scanf("%s %s %d", s[i].name, s[i].subject, &s[i].marks);
+    }
+
+
+    FILE *first;
+    first = fopen("Student.txt", "a"); // "a" append so that old students data isn't erased
+
+        if(first == NULL)
+    {
+        printf("File could not be opened");
+        return 1;
+    }
+
+    for(i = 0; i <n; i++)
+    {
+        fprintf(first, "%s %s %d\n",s[i].name, s[i].subject, s[i].marks);
+    }
+    fclose(first);
+
+    return 0;
+
+
+
+
+}
