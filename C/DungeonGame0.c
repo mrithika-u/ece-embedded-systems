9f30af88b220ea5e50dc2e3c965ec4c43867e189
Mrithika <mrithikaumamahesh005@gmail.com>
2026-05-06 22:54:48 +0530

    Fun games with stick figures and cmd

---
+++ C/DungeonGame0.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+    printf("=========================================\n");
+    printf("             ENTER THE DUNGEON           \n");
+    printf("=========================================\n");
+
+    printf("You are the chosen one.\n");
+    printf("What will you choose?\n");
+    printf("Only one key has the right path: \n");
+    printf(" 0 \n");
+    printf(" 1 \n");
+    printf(" 2 \n");
+
+    Sleep(2000);
+
+    system("cls");
+
+    printf("World 0");
+
+    return 0;
+
+}
+/*
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello");
+
+    system("cls");
+
+    printf("World");
+
+    return 0;
+}
+*/
