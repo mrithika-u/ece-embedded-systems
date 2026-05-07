99960b4a13fa026aaa205670c4e3d8c38afcfc64
Mrithika <mrithikaumamahesh005@gmail.com>
2026-05-07 22:07:56 +0530

    Updated Dungeon Game

--- C/DungeonGame0.c
+++ C/DungeonGame0.c
@@ -4,24 +4,134 @@
 
 int main()
 {
+    int avatar;
     printf("=========================================\n");
     printf("             ENTER THE DUNGEON           \n");
     printf("=========================================\n");
 
-    printf("You are the chosen one.\n");
-    printf("What will you choose?\n");
-    printf("Only one key has the right path: \n");
-    printf(" 0 \n");
-    printf(" 1 \n");
-    printf(" 2 \n");
+    printf("You can choose one of the three avatars:\n");
+    printf(" 0 = o.o  \n");
+    printf(" 1 = $.$ \n");
+    printf(" 2 = ^.^ \n");
 
-    Sleep(2000);
+    scanf("%d",&avatar);
 
+    if(avatar == 0 | avatar == 1 | avatar == 2)
+    {
+    Sleep(500);
     system("cls");
+    Sleep(500);
+    if(avatar == 0)
+    {
+        printf("o.o");
+        Sleep(500);
+        system("cls");
+        printf(" o.o");
+        Sleep(500);
+        system("cls");
+        printf("  o.o");
+        Sleep(500);
+        system("cls");
+        printf("   o.o");
+        Sleep(500);
+        system("cls");
+        printf("    o.o");
+        Sleep(500);
+        system("cls");
+        printf("     o.o");
+        Sleep(500);
+        system("cls");
+        printf("      o.o");
+        Sleep(500);
+        system("cls");
+        printf("       o.o");
+        Sleep(500);
+        system("cls");
+        printf("        o.o");
+        Sleep(500);
+        system("cls");
+        printf("         o.o");
+        Sleep(500);
+        system("cls");
+        return 0;
+    }
+    else if(avatar == 1)
+    {
+        printf("$.$");
+        Sleep(500);
+        system("cls");
+        printf(" $.$");
+        Sleep(500);
+        system("cls");
+        printf("  $.$");
+        Sleep(500);
+        system("cls");
+        printf("   $.$");
+        Sleep(500);
+        system("cls");
+        printf("    $.$");
+        Sleep(500);
+        system("cls");
+        printf("     $.$");
+        Sleep(500);
+        system("cls");
+        printf("      $.$");
+        Sleep(500);
+        system("cls");
+        printf("       $.$");
+        Sleep(500);
+        system("cls");
+        printf("        $.$");
+        Sleep(500);
+        system("cls");
+        printf("         $.$");
+        Sleep(500);
+        system("cls");
+        return 0;
+    }
+    else if(avatar == 2)
+    {
+        printf("^.^");
+        Sleep(500);
+        system("cls");
+        printf(" ^.^");
+        Sleep(500);
+        system("cls");
+        printf("  ^.^");
+        Sleep(500);
+        system("cls");
+        printf("   ^.^");
+        Sleep(500);
+        system("cls");
+        printf("    ^.^");
+        Sleep(500);
+        system("cls");
+        printf("     ^.^");
+        Sleep(500);
+        system("cls");
+        printf("      ^.^");
+        Sleep(500);
+        system("cls");
+        printf("       ^.^");
+        Sleep(500);
+        system("cls");
+        printf("        ^.^");
+        Sleep(500);
+        system("cls");
+        printf("         ^.^");
+        Sleep(500);
+        system("cls");
+        return 0;
+    }
 
-    printf("World 0");
 
-    return 0;
+    }
+    else
+    {
+        printf("You have entered the wrong integer. Go back by pressing space.");
+        return 0;
+    }
+
 
 }
 /*
